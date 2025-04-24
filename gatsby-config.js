0916c72263496561d802569813decd12f881823a
JessicaHoang <jessica97hoang@gmail.com>
2025-04-23 19:35:54 -0700

    added correct social links for github and linkedin, and changed the title and icon of the tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Jessica Hoang',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Jessica is a software engineer who loves building full-stack apps, automating things, and exploring AI and cybersecurity.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        icon: 'src/images/Website_Logo.png',
       },
     },
     `gatsby-plugin-offline`,
